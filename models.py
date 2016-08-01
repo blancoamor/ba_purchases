c36d58bfbceb29ad775956cfdfb9cedb082d16fc
morrillo <gustavo.orrillo@moldeo.coop>
2016-08-01 09:54:17 -0300

    Added invoiced_amount

--- models.py
+++ models.py
@@ -24,7 +24,16 @@ class purchase_order(models.Model):
 				return_value = False
 		self.match_delivered = return_value
 
+	@api.one
+	def _compute_match_invoiced(self):
+		return_value = True
+		for line in self.order_line:
+			if line.product_qty != line.invoiced:
+				return_value = False
+		self.match_invoiced = return_value
+
 	match_delivered = fields.Boolean('Coinciden Entregas',compute=_compute_match_delivered)
+	match_invoiced = fields.Boolean('Coinciden Facturas',compute=_compute_match_invoiced)
 
 class purchase_order_line(models.Model):
 	_inherit = 'purchase.order.line'
@@ -47,5 +56,15 @@ class purchase_order_line(models.Model):
 					return_value = return_value + invoice_line.quantity
 		self.invoiced = return_value		
 
+	@api.one
+	def _compute_invoiced_amount(self):
+		return_value = 0
+		if self.invoice_lines:
+			for invoice_line in self.invoice_lines:
+				if invoice_line.invoice_id.state in ['open','paid']:
+					return_value = return_value + invoice_line.quantity * invoice_line.price_unit
+		self.invoiced_amount = return_value
+
 	delivered = fields.Integer(string='Entregados',compute=_compute_delivered_qty)
 	invoiced = fields.Integer(string='Facturados',compute=_compute_invoiced_qty)
+	amount_invoiced = fields.Float(string='Monto facturado',compute=_compute_invoiced_amount)
